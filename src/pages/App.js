e058842622ae0742fcfea73c3669c0b19a6efb53
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-09-04 13:57:27 +0700

    Set Navigasi / Router

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,25 +1,10 @@
 import React from "react";
 
 import "./App.css";
+import { Routes } from "../configs";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import Home from "./Home";
+import Register from "./Register";
+import Login from "./Login";
+
+export { Home, Register, Login };
